bdcdc9a9e33f6ff0d069fd8d33c2ac1f524e5226
monochromegane <dev.kuro.obi@gmail.com>
2015-08-09 12:16:16 +0900

    Added log file option.

--- main.go
+++ main.go
@@ -13,23 +13,36 @@ var (
 	storage        string
 	dockerHost     string
 	dockerCertPath string
+	logFile        string
 )
 
 func init() {
-	log.SetFormatter(&log.JSONFormatter{})
-	log.SetOutput(os.Stderr)
-
 	defaultPort, _ := strconv.Atoi(os.Getenv("PORT"))
 	flag.IntVar(&port, "port", defaultPort, "port number")
 	flag.StringVar(&storage, "storage", "filesystem", "storage type")
 	flag.StringVar(&dockerHost, "docker-host", os.Getenv("DOCKER_HOST"), "docker host")
 	flag.StringVar(&dockerCertPath, "docker-cert-path", os.Getenv("DOCKER_CERT_PATH"), "docker cert path")
+	flag.StringVar(&logFile, "logfile", "", "log file path")
 	flag.Parse()
+
+	log.SetFormatter(&log.JSONFormatter{})
+
 }
 
 func main() {
+	if logFile != "" {
+		f, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		log.SetOutput(f)
+	} else {
+		log.SetOutput(os.Stdout)
+	}
+
 	err := Run()
 	if err != nil {
-		log.Error(err)
+		log.Fatal(err)
 	}
 }
